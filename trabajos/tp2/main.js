e8e93c0b91bf4bd9974ad9d84d02ba86e976e668
matiasschirone <matiasschirone@gmail.com>
2022-07-25 15:04:46 -0300

    arreglo

--- trabajos/tp2/main.js
+++ trabajos/tp2/main.js
@@ -2,10 +2,11 @@ const Contenedor = require("./contenedor");
 
 const contenedor = new Contenedor( './productos.txt')
 
-contenedor.save({nombe: 'cafe', precio: 20, categoria: 'bebida'});
+//contenedor.save({nombe: 'cafe', precio: 20, categoria: 'bebida'});
 
 // contenedor.getById(5)
 
 // contenedor.getAll()
+contenedor.delete(5)
 
-//contenedor.deleteAll(2)
+//contenedor.deleteAll()
