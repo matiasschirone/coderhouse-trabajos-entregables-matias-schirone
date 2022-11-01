c77b4e0f82c5fd71608cb99bb399e43bf12697f7
matiasschirone <matiasschirone@gmail.com>
2022-11-01 17:36:40 -0300

    arreglos

--- trabajos/tp14/src/server.js
+++ trabajos/tp14/src/server.js
@@ -1,4 +1,4 @@
-import * as dotenv from 'dotenv' 
+import * as dotenv from 'dotenv'
 dotenv.config()
 
 //import parseArgs from 'minimist'
@@ -31,10 +31,10 @@ import productosApiRouter from './routes/api/productos.js'
 import addProductosHandlers from './routes/ws/productos.js'
 import addMensajesHandlers from './routes/ws/mensajes.js'
 
-const cpu = cpus().length
+
 
 //const args = parseArgs(process.argv.slice(2))
-const argv = process.argv.slice(2);
+/*const argv = process.argv.slice(2);
 const parsed = minimist(argv, {
   alias: { p: "port", mo: "mode" },
   default: {
@@ -43,31 +43,48 @@ const parsed = minimist(argv, {
   },
 });
 
-const { port, mode } = parsed;
+const { port, mode } = parsed;*/
+
+const PORT = parseInt(process.argv[2]) || 8080
+const modoCluster = process.argv[3] === 'cluster'
 
 const mongoConfig = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 }
 
-const app = express()
-const httpServer = new HttpServer(app)
-const io = new Socket(httpServer)
+if (modoCluster && cluster.isPrimary) {
+  const numCPUs = cpus().length
 
-io.on('connection', async socket => {
+  console.log(`Numero de CPUs: ${numCPUs}`)
+  console.log(`PID MASTER ${process.pid}`)
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+
+  cluster.on('exit', worker => {
+    console.log(`Worker ${worker.process.pid} died, new Date().toLocaleString()`)
+    cluster.fork()
+  })
+} else {
+  const app = express()
+  const httpServer = new HttpServer(app)
+  const io = new Socket(httpServer)
+
+  io.on('connection', async socket => {
 
     addProductosHandlers(socket, io.sockets)
     addMensajesHandlers(socket, io.sockets)
-});
+  });
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
 
+  //app.use(express.static('public'))
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
+  app.set('view engine', 'ejs');
 
-app.set('view engine', 'ejs');
-
-app.use(session({
+  app.use(session({
     Mongostore: MongoStore.create({ mongoUrl: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/?retryWrites=true&w=majority`, mongoOptions: mongoConfig }),
     client: 'mongodb',
     secret: 'shhhhhhhhhhhhhhhhhhhhh',
@@ -75,20 +92,28 @@ app.use(session({
     saveUninitialized: false,
     rolling: true,
     cookie: {
-        maxAge: 50000
+      maxAge: 50000
     }
-}))
+  }))
+  
+  app.use(compression())
+  
+  app.use(productosApiRouter)
+  
+  app.use(authWebRouter)
+  app.use(homeWebRouter)
+  
 
-app.use(compression())
+  const cpu = cpus().length
+  app.use(infoWebRouter)
+  app.use(routerRandom)
+  httpServer.listen(PORT, () => {
+    console.log(`Servidor express escuchando en el puerto ${PORT}`)
+    console.log(`PID WORKER ${process.pid}`)
+  })
+}
 
-app.use(productosApiRouter)
-
-app.use(authWebRouter)
-app.use(homeWebRouter)
-app.use(infoWebRouter)
-app.use(routerRandom)
-
-if (mode === "fork") {
+/*if (mode === "fork") {
     app.use("/api/randoms", routerRandom);
     httpServer.listen(port, () => {
       console.log(
@@ -113,7 +138,7 @@ if (mode === "fork") {
         );
       });
     }
-}
+}*/
 
 /*const connectedServer = httpServer.listen( process.env.PORT, () => {
     console.log(`Servidor escuchando en el puerto ${connectedServer.address().port}`)
