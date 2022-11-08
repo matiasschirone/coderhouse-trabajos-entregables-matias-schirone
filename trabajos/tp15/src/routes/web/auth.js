035823ed35dbe143c4c310cd45e321f98e08fa32
matiasschirone <matiasschirone@gmail.com>
2022-11-08 10:29:00 -0300

    heroku

--- trabajos/tp15/src/routes/web/auth.js
+++ trabajos/tp15/src/routes/web/auth.js
@@ -1,16 +1,10 @@
 import { Router } from 'express'
 
-import crypto from 'crypto'
-
 import path from 'path'
 
 const authWebRouter = new Router()
 
-const users = {}
-
-app.use(express.static('public'))
-
-/*authWebRouter.get('/', (req, res) => {
+authWebRouter.get('/', (req, res) => {
     res.redirect('/home')
 })
 
@@ -21,76 +15,7 @@ authWebRouter.get('/login', (req, res) => {
     } else {
         res.sendFile(path.join(process.cwd(), '/views/login.html'))
     }
-})*/
-
-authWebRouter.get("/getUsers", (req, res) => {
-    res.json({ users })
-  })
-  
-  app.get("/newUser", (req, res) => {
-    let username = req.query.username || "";
-    const password = req.query.password || "";
-  
-    username = username.replace(/[!@#$%^&*]/g, "");
-  
-    if (!username || !password || users[username]) {
-      return res.sendStatus(400);
-    }
-  
-    const salt = crypto.randomBytes(128).toString("base64");
-    const hash = crypto.pbkdf2Sync(password, salt, 10000, 512, "sha512");
-  
-    users[username] = { salt, hash };
-  
-    res.sendStatus(200);
-});
-  
-
-authWebRouter.get("/auth-bloq", (req, res) => {
-    let username = req.query.username || "";
-    const password = req.query.password || "";
-  
-    username = username.replace(/[!@#$%^&*]/g, "");
-  
-    if (!username || !password || !users[username]) {
-      process.exit(1)
-      // return res.sendStatus(400);
-    }
-  
-    const { salt, hash } = users[username];
-    const encryptHash = crypto.pbkdf2Sync(password, salt, 10000, 512, "sha512");
-  
-    if (crypto.timingSafeEqual(hash, encryptHash)) {
-      res.sendStatus(200);
-    } else {
-      process.exit(1)
-      // res.sendStatus(401);
-    }
-});
-  
-
-authWebRouter.get("/auth-nobloq", (req, res) => {
-    let username = req.query.username || "";
-    const password = req.query.password || "";
-    username = username.replace(/[!@#$%^&*]/g, "");
-  
-    if (!username || !password || !users[username]) {
-      process.exit(1)
-      // return res.sendStatus(400);
-    }
-    crypto.pbkdf2(password, users[username].salt, 10000, 512, 'sha512', (err, hash) => {
-      if (users[username].hash.toString() === hash.toString()) {
-        res.sendStatus(200);
-      } else {
-        process.exit(1)
-        //res.sendStatus(401);
-      }
-    });
-});
-  
-  
-
-
+})
 
 authWebRouter.get('/logout', (req, res) => {
     const nombre = req.session?.nombre
--- trabajos/tp15/src/server.js
+++ trabajos/tp15/src/server.js
@@ -1,80 +1,44 @@
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv' 
 dotenv.config()
+
 import express from 'express'
 import session from 'express-session'
 import MongoStore from 'connect-mongo'
-
-//import parseArgs from 'minimist'
-import minimist from 'minimist'
-import cluster from 'cluster'
-import http from 'http'
-import { cpus } from 'os'
-
-
-import { fork } from 'child_process'
-
-import compression from 'compression'
-
-
-import config from './config.js'
 
 
 import { Server as HttpServer } from 'http'
 import { Server as Socket } from 'socket.io'
 
 import authWebRouter from './routes/web/auth.js'
-import productosWebRouter from './routes/web/home.js'
-import routerRandom from './routes/web/ramdom.js'
+import homeWebRouter from './routes/web/home.js'
 import productosApiRouter from './routes/api/productos.js'
 
 import addProductosHandlers from './routes/ws/productos.js'
 import addMensajesHandlers from './routes/ws/mensajes.js'
-import infoRouter from './routes/web/info.js'
-import logger from './logs/logger.js'
 
-
-const PORT = parseInt(process.argv[2]) || 8080
-const modoCluster = process.argv[3] == 'cluster'
-
-
-if (modoCluster && cluster.isPrimary) {
-  const numCPUs = cpus().length
-  
-  console.log(`Numero de CPUs: ${numCPUs}`)
-  console.log(`PID MASTER ${process.pid}`)
-  
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork()
-  }
-  
-  cluster.on('exit', worker => {
-    console.log(`Worker ${worker.process.pid} died, new Date().toLocaleString()`)
-    cluster.fork()
-  })
-} else {
-
-  const app = express()
-  const httpServer = new HttpServer(app)
-  const io = new Socket(httpServer)
-  
-  io.on('connection', async socket => {
-    
-    addProductosHandlers(socket, io.sockets)
-    addMensajesHandlers(socket, io.sockets)
-  });
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: true }))
-  
-  app.use(express.static('public'))
-  
-  app.set('view engine', 'ejs');
-  
-  const mongoConfig = {
+const mongoConfig = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }
+}
 
-  app.use(session({
+const app = express()
+const httpServer = new HttpServer(app)
+const io = new Socket(httpServer)
+
+io.on('connection', async socket => {
+
+    addProductosHandlers(socket, io.sockets)
+    addMensajesHandlers(socket, io.sockets)
+});
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
+
+app.use(session({
     Mongostore: MongoStore.create({ mongoUrl: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/?retryWrites=true&w=majority`, mongoOptions: mongoConfig }),
     client: 'mongodb',
     secret: 'shhhhhhhhhhhhhhhhhhhhh',
@@ -82,60 +46,16 @@ if (modoCluster && cluster.isPrimary) {
     saveUninitialized: false,
     rolling: true,
     cookie: {
-      maxAge: 50000
+        maxAge: 50000
     }
-  }))
-  
-  //app.use(compression())
-  
-  app.use(productosApiRouter)
-  
-  app.use(authWebRouter)
-  app.use(productosWebRouter)
-  
-  app.use(infoRouter)
-  app.use(routerRandom)
+}))
 
-  app.use((req,res,next)=>{
-    logger.warn("NONE EXISTING URL");
-    res.sendStatus('404')
-  })
+app.use(productosApiRouter)
 
+app.use(authWebRouter)
+app.use(homeWebRouter)
 
-  app.listen(PORT, () => {
-    console.log(`Servidor express escuchando en el puerto ${PORT}`)
-    console.log(`PID WORKER ${process.pid}`)
-  })
-}
-
-/*if (mode === "fork") {
-    app.use("/api/randoms", routerRandom);
-    httpServer.listen(port, () => {
-      console.log(
-        `ESTOY CORRIENDO EN  EL MODO FORK EN EL PUERTO : http://localhost:${port}`
-      );
-    });
-  }
-  if (mode !== "fork") {
-    if (cluster.isPrimary) {
-      console.log(`Master ${process.pid} is running`)
-      for (let i = 0; i < numCPUs; i++) {
-          cluster.fork()
-      }
-      cluster.on('exit', (worker, code, signal) => {
-          console.log(`worker ${worker.process.pid} died`)
-      })  
-    } else {
-      app.use("/info", routerRandom);
-      httpServer.listen(port, () => {
-        console.log(
-          `ESTOY CLUSTER CORRIENDO EN MODO CLUSTER EL PUERTO : http://localhost:${port}`
-        );
-      });
-    }
-}*/
-
-/*const connectedServer = httpServer.listen( process.env.PORT, () => {
+const connectedServer = httpServer.listen( process.env.PORT, () => {
     console.log(`Servidor escuchando en el puerto ${connectedServer.address().port}`)
 })
-connectedServer.on('error', error => console.log(`Error en servidor ${error}`))*/
+connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
