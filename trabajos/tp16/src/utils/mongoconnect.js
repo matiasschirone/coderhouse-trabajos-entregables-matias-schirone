5b88159a9ecbfd106a7903c88eb264c75bbf9296
matiasschirone <matiasschirone@gmail.com>
2022-11-21 23:15:06 -0300

    actualizacion

---
+++ trabajos/tp16/src/utils/mongoconnect.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv").config();
+
+const mongoConnect = async () => {
+	try {
+		const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/?retryWrites=true&w=majority`;
+		mongoose.connect(url, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		});
+		console.log("MongoDb conectado");
+	} catch (error) {
+		console.error(`error de conexion: ${error}`);
+	}
+};
+
+module.exports = mongoConnect;
